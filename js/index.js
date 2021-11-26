8ec0f60889a0149880b5ed0730d9ec2c2bb9b11c
M-Haseeb-Akram <ha832440@gmail.com>
2021-11-26 16:16:20 +0500

    changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
-let key_values = (element) => {
-
-}
-
 const buttons = document.querySelectorAll(".calc-keys button");
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 for (const button of buttons) { 
-  button.addEventListener('click', function(event) {
+  button.addEventListener('click', event => {
     if(button.getAttribute('id') == 'clear'){
         clear();
     }
@@ -19,6 +15,21 @@ for (const button of buttons) {
   });
 }
 
+document.addEventListener('keydown', event => {
+    const name = event.key;
+    const keyCode = event.keyCode || event.which;
+    if((keyCode >=48 && keyCode <=57) || name === '*' || name === '+' || name === '-' || name === '/' || name === '(' || name === ')'){
+        input.innerText += name;
+    } 
+    else if (name === 'Backspace' || name === 'Delete'){
+        input.innerText = input.innerText.substring(0,input.innerText.length-1);
+    }
+    else if(name === 'Enter'){
+        equal();
+    }
+  }, false);
+
+
 let clear = () => {
     input.innerText = "";
     output.innerText = "";
@@ -27,7 +38,9 @@ let clear = () => {
 let equal = () => {
     try{
         let result = eval(input.innerText.trim())
-        output.innerText = result;
+        if(result != undefined){
+            output.innerText = result;
+        }   
     }
     catch(err){
         output.innerText = "Invalid";   
